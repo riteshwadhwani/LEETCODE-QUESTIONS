1fd0f7b49c76f8a47e4584ca2d4a12b8aeac2b00
Ritesh Wadhwani <119737525+riteshwadhwani@users.noreply.github.com>
2023-08-08 21:42:23 +0530

    Time: 4 ms (44.66%), Space: 11.3 MB (13.70%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -0,0 +1,67 @@
+class Solution {
+public:
+int findPivotIndex(vector<int>arr){
+    int s =0; 
+    int e =arr.size()-1;
+    int mid = s + (e-s)/2;
+    while (s<=e){
+         if(s==e){
+            return s;
+        }
+      if(mid+1<=arr.size() && arr[mid]>arr[mid+1]){
+          return mid;
+      }
+      if(mid-1>=0 && arr[mid-1]>arr[mid]){
+          return mid-1;
+      }
+
+      if(arr[s]>arr[mid]){
+          e = mid-1;
+      }
+      else{
+          s = mid+1;
+      }
+       mid = s + (e-s)/2;
+
+
+    }
+    return -1;
+}
+int binarySearch(vector<int>arr,int s, int e,int target){
+    
+    int mid = s + (e-s)/2;
+    while(s<=e){
+        if(target==arr[mid]){
+            return mid;
+        }
+        if(target>arr[mid]){
+            s = mid+1;
+
+        }   
+        else{
+            e=mid-1;
+        }
+        mid = s+ (e-s)/2;
+        
+         }
+         return -1;
+}
+    int search(vector<int>& nums, int target) {
+        int pivotIndex = findPivotIndex(nums);
+        cout<<pivotIndex;
+
+        int ans =-1;
+        if (target >= nums[0] && target<=nums[pivotIndex]){
+            ans = binarySearch(nums, 0, pivotIndex,target);
+            return ans;
+
+        }
+        else{
+            ans = binarySearch(nums, pivotIndex+1, nums.size()-1, target);
+            return ans;
+        }
+        return ans;
+
+
+    }
+};
