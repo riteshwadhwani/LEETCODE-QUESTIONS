837dbade1c90a60551fd1996a4e1010236d207f7
Ritesh Wadhwani <119737525+riteshwadhwani@users.noreply.github.com>
2023-08-19 15:56:09 +0530

    Time: 5 ms (59.88%), Space: 6.8 MB (57.20%) - LeetHub

--- 0415-add-strings/0415-add-strings.cpp
+++ 0415-add-strings/0415-add-strings.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
-    string addS(string &num1,int p1, string &num2, int p2, int carry){
+    void addS(string &num1,int p1, string &num2, int p2, int carry,string&ans){
         
         if(p1<0 && p2<0){
             if(carry!=0){
-                return string(1,carry +'0');
+                ans.push_back(carry +'0');
             }
-            return "";
+            return;
         }
-        string ans = "";
+       
         int n1 = (p1>=0 ? num1[p1]:'0')-'0';
         int n2 = (p2>=0 ? num2[p2]:'0')-'0';
         int sum = n1 + n2 + carry;
         carry = sum/10;
         ans.push_back(sum%10+'0');
-        ans += addS(num1, p1-1, num2, p2-1, carry);
-        return ans;
+        addS(num1, p1-1, num2, p2-1, carry,ans);
         
     }
     string addStrings(string num1, string num2) {
         int p1 = num1.size()-1;
         int p2 = num2.size()-1;
-        string ans = addS(num1,p1, num2,p2,0);
+        string ans ="";
+        addS(num1,p1, num2,p2,0 , ans);
         reverse(ans.begin(),ans.end());
         return ans;
         
