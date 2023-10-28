711603c742f3e3fef2584c723e6de837bd4c215f
Ritesh Wadhwani <119737525+riteshwadhwani@users.noreply.github.com>
2023-10-28 10:14:49 +0530

    Time: 250 ms (27.57%), Space: 149.1 MB (24.53%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,24 +1,37 @@
-
+class node{
+	public:
+	int data;
+	int index;
+	node(int d , int i){
+		data = d;
+		index = i;
+	}
+};
+class compare{
+	public:
+	bool operator()(node a , node b){
+		return a.data < b.data;
+	}
+};
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-     priority_queue<pair<int,int>>maxHeap;
+     priority_queue<node,vector<node>,compare>maxHeap;
         vector<int>ans;
         if(k==0){
             return ans;
         }
-        int maxi=INT_MIN;
         
         for(int i =0;i<k;i++){
-            maxHeap.push({nums[i],i});
+            maxHeap.push(node(nums[i],i));
         }
-      ans.push_back(maxHeap.top().first);
+      ans.push_back(maxHeap.top().data);
         for(int i =k ;i<nums.size();i++){
-             maxHeap.push({nums[i],i});
-            while(maxHeap.top().second<=i-k){
+             maxHeap.push(node(nums[i],i));
+            while(maxHeap.top().index<=i-k){
                 maxHeap.pop();
             }
-             ans.push_back(maxHeap.top().first);
+             ans.push_back(maxHeap.top().data);
         }
     
   
